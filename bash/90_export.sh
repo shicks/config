be92d3608379841c4a505ede565a6eabf5e9639d
Stephen Hicks <sdh33@cornell.edu>
2013-06-22 15:39:02 -0700

    make LESSPIPE and LESSOPEN use which to find lesspipe tool

--- bash/90_export.sh
+++ bash/90_export.sh
@@ -10,7 +10,7 @@ export PATH=$HOME/local/bin:$PATH
 
 # Use our custom lesspipe, with syntax highlighting
 # See http://linux-tips.org/article/78/syntax-highlighting-in-less
-export LESSPIPE="$HOME/local/bin/lesspipe %s"
-export LESSOPEN="|$HOME/local/bin/lesspipe %s"
+export LESSPIPE="$(which lesspipe) %s"
+export LESSOPEN="|$LESSPIPE"
 export LESS='-R -F -X'  # allow ANSI colors, exit if <1 page, disable ti/te
 
