b0075bc59e8f6742577dacc9c1ed680da509d009
Stephen Hicks <stephenhicks@gmail.com>
2022-10-17 00:22:27 -0400

    add elisp macro sdh-increment-number-at-point

--- emacs/sdh-misc.el
+++ emacs/sdh-misc.el
@@ -919,4 +919,14 @@ Does nothing if already in the window system."
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; from https://www.emacswiki.org/emacs/IncrementNumber
+(defun sdh-increment-number-at-point ()
+  (interactive)
+  (skip-chars-backward "0-9")
+  (or (looking-at "[0-9]+")
+      (error "No number at point"))
+  (replace-match (number-to-string (1+ (string-to-number (match-string 0))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (provide 'sdh-misc)
