2c390f732c004c3a5c45dc1b52a213e924729b22
Stephen Hicks <stephenhicks@gmail.com>
2026-05-03 21:32:27 -0400

    add gcloud.zsh

---
+++ sh/gcloud.zsh
@@ -0,0 +1,5 @@
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/sdh/local/opt/google-cloud-sdk/path.zsh.inc' ]; then . '/home/sdh/local/opt/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/home/sdh/local/opt/google-cloud-sdk/completion.zsh.inc' ]; then . '/home/sdh/local/opt/google-cloud-sdk/completion.zsh.inc'; fi
